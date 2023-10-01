184cc8ce2d8e25a6a8c7c90440cab1c2aed44e92
Tom Hinkle <tmhinkle@gmail.com>
2023-10-01 10:36:40 -0400

    ++score based on playable words ++colors for in-between scores

--- boggle-fall/src/words.ts
+++ boggle-fall/src/words.ts
@@ -1,32 +1,34 @@
-import wordList from './wordsEn.txt?raw';
-import { letterbank } from './stores';
-let words = wordList.split('\n').map((line)=>line.replace(/\s/g,''));
+import wordList from "./wordsEn.txt?raw";
+import { letterbank } from "./stores";
+let words = wordList.split("\n").map((line) => line.replace(/\s/g, ""));
 
-export function isWord (word : string) {  
-  if (words.find((w)=>w==word)) {
+export function isWord(word: string) {
+  if (words.find((w) => w == word)) {
     return true;
   } else {
     return false;
   }
 }
 
-export let letterValues : {[key : string] : number} = {  
-};
-
-let frequencies : {[key: string] : number} = {}
-for (let letter of letterbank) {
-  if (!frequencies[letter]) {
-    frequencies[letter] = 1;
-  } else {
-    frequencies[letter] += 1;
+export let letterValues: { [key: string]: number } = {};
+let letterCount = 0;
+let frequencies: { [key: string]: number } = {};
+for (let word of wordList) {
+  for (let letter of word) {
+    letterCount++;
+    if (!frequencies[letter]) {
+      frequencies[letter] = 1;
+    } else {
+      frequencies[letter] += 1;
+    }
   }
 }
 let rarest = 10000;
-let rarestLetter = ''
+let rarestLetter = "";
 let mostCommon = 0;
-let mostCommonLetter = ''
+let mostCommonLetter = "";
 for (let l in frequencies) {
-  let occurs = frequencies[l]
+  let occurs = frequencies[l];
   if (occurs < rarest) {
     rarest = occurs;
     rarestLetter = l;
@@ -36,18 +38,52 @@ for (let l in frequencies) {
     mostCommonLetter = l;
   }
 }
-console.log('Rarest letter is',rarestLetter,rarest);
-console.log('Most common is',mostCommonLetter,mostCommon);
-console.log('Frequencies are',frequencies);
+console.log("Rarest letter is", rarestLetter, rarest);
+console.log("Most common is", mostCommonLetter, mostCommon);
+console.log("Frequencies are", frequencies);
 
 let letters = Object.keys(frequencies);
-letters.sort((a,b)=>frequencies[a]-frequencies[b]);
 
+// Our frequencies range from .15% to 10%...
+for (let l of letters) {
+  let pct = (100 * frequencies[l]) / letterCount;
+  let score: number;
+  if (pct > 6) {
+    score = 1;
+  } else if (pct > 4) {
+    score = 2;
+  } else if (pct > 2.5) {
+    score = 3;
+  } else if (pct > 1.5) {
+    score = 4;
+  } else if (pct > 1) {
+    score = 5;
+  } else if (pct > 0.8) {
+    score = 6;
+  } else if (pct > 0.6) {
+    score = 7;
+  } else if (pct > 0.25) {
+    score = 8;
+  } else if (pct > 0.16) {
+    score = 9;
+  } else {
+    score = 10;
+  }
+  console.log(l, pct.toFixed(3), "%", "=>", score);
+  letterValues[l] = score;
+}
+
+//letters.sort((a,b)=>frequencies[a]-frequencies[b]);
+/* 
 for (let idx=0; idx<letters.length; idx++) {
   let score = 1;
-  if (idx < 2) {
+  // Rank our 26 letters and distribute them as follows...
+  if (idx < 2) { // 2 10-pointers
     score = 10;  
-  } else if (idx < 5) {
+  } else if (idx < 3) {
+    score = 9;
+  } 
+  else if (idx < 5) {
     score = 8;
   } else if (idx < 9) {
     score = 6
@@ -62,18 +98,17 @@ for (let idx=0; idx<letters.length; idx++) {
   } else {
     score = 1
   }
-  console.log(letters[idx],'=>',score)
+  console.log(letters[idx],'=>',score, '(',100*frequencies[letters[idx]]/letterCount,')')
   letterValues[letters[idx]] = score;
-}
+} */
 
-
-export function scoreWord (word : string) {
+export function scoreWord(word: string) {
   let value = 0;
   for (let ltr of word) {
-    value += letterValues[ltr]||0;
+    value += letterValues[ltr] || 0;
   }
   let lengthMultiplier = getMultiplier(word);
-  return Math.ceil(value*lengthMultiplier);
+  return Math.ceil(value * lengthMultiplier);
 }
 
 export function getMultiplier(word: string) {
@@ -92,28 +127,28 @@ export function getMultiplier(word: string) {
   }
   if (lengthMultiplier > 2) {
     // No fractions bigger...
-    lengthMultiplier = Math.ceil(lengthMultiplier)
+    lengthMultiplier = Math.ceil(lengthMultiplier);
   }
   return lengthMultiplier;
 }
 
-let word = 'h'
-for (let i=1; i<25; i++) {
-  word += 'i';
-  console.log('Word of length',i,'multiplier=',getMultiplier(word))
+let word = "h";
+for (let i = 1; i < 25; i++) {
+  word += "i";
+  console.log("Word of length", i, "multiplier=", getMultiplier(word));
 }
 
 let deadEnds = new Set();
 
-export function findMatchingWords (stem : string, wordlist=null) {
+export function findMatchingWords(stem: string, wordlist = null) {
   let ww = wordlist || words;
   if (deadEnds.has(stem)) {
     return null;
   } else {
-    let matches = ww.filter((w)=>w.substring(0,stem.length)==stem)
-    if (matches.length==0) {
+    let matches = ww.filter((w) => w.substring(0, stem.length) == stem);
+    if (matches.length == 0) {
       deadEnds.add(stem);
-      return null
+      return null;
     } else {
       return matches;
     }
