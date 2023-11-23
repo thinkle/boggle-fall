0c0869f8b6b9ed1ec075e89d330ffb5b35916962
Tom Hinkle <tmhinkle@gmail.com>
2023-11-23 10:21:20 -0500

    Back out breaking change

--- boggle-fall/vite.config.ts
+++ boggle-fall/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import babel from "vite-plugin-babel";
+//import babel from "vite-plugin-babel";
 
 export default defineConfig({
-  plugins: [svelte(), babel()],
+  plugins: [
+    svelte(),
+    //babel()
+    // Currently not so much working :-(
+  ],
 });
